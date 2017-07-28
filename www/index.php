8aad2a5a996911a33bfe049bfcfb8ea80878f9a2
Andrei Timotin <andreitimotin11@gmail.com>
2017-07-28 19:43:46 +0300

    test

--- www/index.php
+++ www/index.php
@@ -8,10 +8,36 @@
 ?>
 <html>
 <head>
-	<title>OOP</title>
+    <title>OOP</title>
 </head>
 <body>
 <?php
+
+
+class Table
+{
+	public $legs;
+	public $color;
+	public function show(){
+	    echo "i'm a table ";
+	    echo $this->color;
+    }
+}
+
+$table = new Table();
+$table->color = 'red';
+$table->legs = 4;
+$table->show();
+echo $table->color;
+echo $table->legs;
+$table2 = new Table();
+$table2->color = 'white';
+echo $table2->color;
+
+//var_dump($table);
+
+//echo $table->color;
+//echo $table2->color;
 
 ?>
 </body>
